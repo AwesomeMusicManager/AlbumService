916952321220e8beae4938b847bb718658453c53
Yan Alves <yanalves@hotmail.com>
2019-12-09 01:55:22 -0300

    :construction: Add model and service layers

---
+++ AwesomeMusicManager.AlbumService.Service/AwesomeMusicManager.AlbumService.Service/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace AwesomeMusicManager.AlbumService.Service
+{
+    public class Class1
+    {
+    }
+}
---
+++ AwesomeMusicManager.AlbumService/AwesomeMusicManager.AlbumService.Model/Class1.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace AwesomeMusicManager.AlbumService.Model
+{
+    public class Album
+    {
+        public string Type { get; set; }
+    }
+}
